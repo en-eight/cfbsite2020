4cc47ea1008b91cf9a97d2173a144011bdef0dbd
nholcom <nholcom@bgsu.edu>
2020-02-15 14:09:30 -0500

    Added template, src dirs

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Community First Bank, N.A. index.php v2.0 by Nate Holcomb -->
+<!-- Feb. 15th, 2020 --> 
+
+<!--Description: index.php - PageName - Community First Bank, N.A.-->
+
+<head>
+  <title> Home - Community First Bank, N.A.</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
