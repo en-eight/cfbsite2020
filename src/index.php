a50d69773d74673fa1fb85dc0a26380fca250082
Nate Holcomb <sadhorseman@pm.me>
2020-03-05 16:43:20 -0500

    Added jumbotron + 2 column layout to index.php

--- src/index.php
+++ src/index.php
@@ -20,24 +20,65 @@
 <div class="container-fluid">
 
 	<!-- CREATING THE NAVBAR -->
-	<div class = "navbar navbar-expand-sm bg-dark">
-		<ul class = "navbar-nav">
-			<li class = "nav-item">
-				<a class = "nav-link" href="home.php"> Home </a>
-			</li>
-			<li class = "nav-item">
-				<a class = "nav-link" href="loans.php"> Loans </a>
-			</li>
-			<li class = "nav-item">
-				<a class = "nav-link" href="newsletter.php"> Coffee Chats </a>
-			</li>
-			<li class = "nav-item">
-				<a class = "nav-link" href = "about.php"> About Us </a>
-			</li>
-		</ul>
-	</div>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<a class="navbar-brand" href="#">Navbar</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNavDropdown">
+			<ul class="navbar-nav">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+				</li>
+				
+				<li class="nav-item">
+					<a class="nav-link" href="#">Features</a>
+				</li>
+				
+				<li class="nav-item">
+					<a class="nav-link" href="#">Pricing</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Dropdown link
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+						<a class="dropdown-item" href="#">Action</a>
+						<a class="dropdown-item" href="#">Another action</a>
+						<a class="dropdown-item" href="#">Something else here</a>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</nav>
 	<!-- END NAVBAR CREATION -->
-
+	
+	
+	
+	
+	
+	<!--begin main content -->
+	<div class = "jumbotron text-center">
+		<h1> Community First Bank, N.A.</h1>
+		<p> Where the grass stays greener. </p>
+	</div>
+	
+	<div class = "container">
+		<div class="row">
+			<div class="col-sm-6 text-center">
+				<h3> Looking to finance a new project? </h3>
+				<p> Lorem ipsum sit amet, consectetur adipisicing elit...</p>
+				<p> Awesome, made in Bootstrap, and incredibly readable. </p>
+			</div>
+			<div class="col-sm-6 text-center">
+				<h3> Or maybe something else? </h3>
+				<p> Lorem ipsum sit amet, consectetur adipisicing elit...</p>
+				<p> Awesome, made in Bootstrap, and incredibly readable. </p>
+			</div>
+		</div>
+	</div>
+	
+	
 </div>
 
 </html>
