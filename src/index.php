4e3fc389039c4587d610947859704058b62eb2a7
Nate Holcomb <sadhorseman@pm.me>
2020-03-05 16:46:34 -0500

    added lorem ipsum to index.php

--- src/index.php
+++ src/index.php
@@ -67,12 +67,18 @@
 		<div class="row">
 			<div class="col-sm-6 text-center">
 				<h3> Looking to finance a new project? </h3>
-				<p> Lorem ipsum sit amet, consectetur adipisicing elit...</p>
+				<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+				Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+				Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+				Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				<p> Awesome, made in Bootstrap, and incredibly readable. </p>
 			</div>
 			<div class="col-sm-6 text-center">
 				<h3> Or maybe something else? </h3>
-				<p> Lorem ipsum sit amet, consectetur adipisicing elit...</p>
+				<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+				Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+				Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+				Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				<p> Awesome, made in Bootstrap, and incredibly readable. </p>
 			</div>
 		</div>
