d307173f2e90da8b56a90f99cb0f31b09b830aad
Nate Holcomb <sadhorseman@pm.me>
2020-04-02 02:49:27 -0400

    Add about.php

---
+++ src/about.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Community First Bank, N.A. title.php v2.0 by Nate Holcomb -->
+<!-- Feb. 15th, 2020 --> 
+
+<!--Description: about.php - About Us - Community First Bank, N.A.-->
+<head>
+	<title> About Us - Community First Bank, N.A. </title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- BOOTSTRAP LINK, GOOGLE API LINK, JQUERY/JS LINK-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<!-- end linking. if you want to add more files, declarations go below -->
+</head>
+
+<!--MAKING THE MAIN CONTAINER-->
+<div class="container-fluid no-pad">
+
+	<!-- making the navbar -->
+	<div class="header">
+		<a href="index.php">
+			<img id="logo" src="media/lnl.png" alt="Community 1st Bank">
+		</a>
+		<h3 id="padtext"><b>Community First Bank, N.A.</b></h3>
+		<div class="topnav">
+			<a href="safety.php">Financial Safety</a>
+			<a href="loans.php">Loans and Lending</a>
+			<a href="newsletter.php">Coffee Chats</a>
+			<a href="about.php">About Us</a>
+			<a href="index.php">Home</a>
+		</div>
+	</div>
+
+	<!--begin main content -->
+	<div class "jumbotron text-center">
+		<h1> About Us </h1>
+	</div>
+	<div class = "text-center">
+		<p> Community First Bank, N.A. was founded in 1904. 
+		The bank was then known as the First National Bank of Forest, Ohio. 
+		In 1981, a branch opened in the neighboring community of Kenton, Ohio. 
+		At that time, the bank changed its name to Community First Bank, National Association. 
+		The reasoning behind the name change was simple: 
+		Community First is a local bank for all our communities where the customers are “First With Us!”   
+		In 2002, Community First opened another office in the heart of downtown Upper Sandusky, Ohio. </p>
+			
+		<p> Community First Bank, N.A. is a locally owned, controlled, family oriented bank, dedicated to
+		providing a safe, secure, competitive source of financial services to the residents of our community,
+		small businesses, and family farms in our market area. It is further our mission to provide our
+		shareholders with an adequate return on their investment in terms of both short-term yield and long-term
+		growth while preserving a well-capitalized corporation. It is our mission to offer our employees quality
+		employment where they are treated with respect and evaluated on their contributions to the goals and
+		objectives of Community First Bank, N.A. </p>
+	</div>
+		
+	<div class = "row">
+		<div class = "col-sm-6 text-center">
+			<h3> Hours of Operation </h3>
+			<p> Placeholder text - will update with table soon </p>
+		</div>
+		<div class = "col-sm-6 text-center">
+			<!-- Update to include addresses. Will need table layout. --> 
+			<h3> Locations </h3>
+			<p> Forest, Ohio - 419.273.2595</p>
+			<p> Kenton, Ohio - 419.673.1084 </p>
+			<p> Upper Sandusky, Ohio - 419.209.0308 </p>
+		</div>
+	</div>
+	
+	<br>
+	<br>
+	<br>
+	
+	<footer class="footer">
+		<div class = "container">
+			<span class = "text-muted"> Copyright 2020 Community First Bank, N.A.</span>
+		</div>
+	</footer>
+
+</div>
+
+</html>
